2d790cbd182856e688f983cd5c146eb01806d48e
gokul-khoros <gokulreko@gmail.com>
2020-11-11 00:03:13 +0530

    few notes are added

--- src/SecondApplication.java
+++ src/SecondApplication.java
@@ -3,6 +3,8 @@ package src;
 public class SecondApplication {
 
     public String sideKick(){
+        String learnt = "git log can be used to see all the commit history, those hash value can be used for checkout";
+        String phoebe = "ssh-keyegn along with rsa encrption can be used for authentication";
         String joeyTribbiani = "hey how u dong";
         return joeyTribbiani;
     }
