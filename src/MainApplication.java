40d2e43a4e5a74580949493fd55252533678322c
gokul-khoros <gokulreko@gmail.com>
2020-11-12 01:35:55 +0530

    cherry pick used in our file

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -3,5 +3,8 @@ package src;
 public class MainApplication {
         public static void main(String[] args) {
             System.out.println("bonjour");
+
+            System.out.println("hola hola hola");
+
         }
 }
