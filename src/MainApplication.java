618157db7375d544ceba6c1568b656c95f534ca1
gokul-khoros <gokulreko@gmail.com>
2020-11-12 01:32:09 +0530

    using for cherrypick

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -3,6 +3,7 @@ package src;
 public class MainApplication {
         public static void main(String[] args) {
             System.out.println("bonjour");
-            System.out.println("hola");
+            System.out.println("hola hola hola");
+
         }
 }
