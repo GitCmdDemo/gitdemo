173abe98fa513190a7e1dbd2242b6f24940153d8
gokul-khoros <gokulreko@gmail.com>
2020-11-12 01:39:54 +0530

    Revert "cherry pick used in our file"
    This reverts commit 40d2e43a4e5a74580949493fd55252533678322c.

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -3,8 +3,5 @@ package src;
 public class MainApplication {
         public static void main(String[] args) {
             System.out.println("bonjour");
-
-            System.out.println("hola hola hola");
-
         }
 }
